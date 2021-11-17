08b79c7164e5e672758af0fe593d229f42fef37d
NicolasLF4 <lafuentejoel2002@gmail.com>
2021-11-17 19:11:01 -0300

    arreglo de register y login

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <link rel="icon" href="img/logo.png" >
     <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="css/index.css">
+    <!-- <link rel="stylesheet" href="css/index.css"> -->
 </head>
 
 <body>
